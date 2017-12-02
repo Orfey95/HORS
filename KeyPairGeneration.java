e6c669bfe925fa8cbf829e2fe67e522161b5f194
Sasha <sasha7692@gmail.com>
2017-12-02 23:29:52 +0200

    Update KeyPairGeneration.java

--- KeyPairGeneration.java
+++ KeyPairGeneration.java
@@ -1,23 +1,21 @@
 import Config.*;
+
 public class KeyPairGeneration {
     PRG prg = new PRG();
-    MD5Binary md5B = new MD5Binary();
+    MD5HEX md5H = new MD5HEX();
 
     public static String X = "";
     public static String Y = "";
 
-
-
-    public void generatePairKey(Integer t) {
+    public void generatePairKey(Integer t, Integer l) {
         String Xi = "";
         String Yi = "";
 
         for(int i = 1; i <= t; i++){
-            Xi = prg.Random128();
+            Xi = prg.Random128(l);
             X += Xi;
-            Yi = md5B.md5Custom(Xi);
+            Yi = md5H.md5Custom(Xi);
             Y += Yi;
         }
     }
-
 }
