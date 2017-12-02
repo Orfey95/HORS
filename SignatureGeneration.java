ce432ed4db321a741482d228dc0fc7442ead9d2a
Sasha <sasha7692@gmail.com>
2017-12-02 20:26:28 +0200

    Update SignatureGeneration.java

--- SignatureGeneration.java
+++ SignatureGeneration.java
@@ -4,23 +4,23 @@ public class SignatureGeneration {
     Binarylog bl = new Binarylog();
     MD5Binary md5B = new MD5Binary();
 
-    public String hashMessage(String Message){ //Хэширование сообщения
+    public String hashMessage(String Message){ // Message hashing
         Message = md5B.md5Custom(Message);
         return Message;
     }
 
-    public Integer [] separateMessage(String Message, Integer k, Integer t){ // Разделение сообщение на блоки
+    public Integer [] separateMessage(String Message, Integer k, Integer t){ // Separation of the message into blocks
         Message = hashMessage(Message);
         Integer [] blocksOfMessage = new Integer[k];
-        int j = 0;  //счетчик для индексов массива
-        for(int i = 0; i < Message.length(); i = i + (int)Math.ceil(bl.binlog((double) t))) { // проход по массиву через каждые w символа для нахождения новой подстроки
-            String S1 = Message.substring(i, i + (int)Math.ceil(bl.binlog((double) t))); // нахождение подстроки с длиной в w символа
-            blocksOfMessage[j++] = Integer.parseInt(S1, 2); // присваивание подстроки к элементу массива
+        int j = 0;
+        for(int i = 0; i < Message.length(); i = i + (int)Math.ceil(bl.binlog((double) t))) {
+            String S1 = Message.substring(i, i + (int)Math.ceil(bl.binlog((double) t)));
+            blocksOfMessage[j++] = Integer.parseInt(S1, 2);
         }
         return blocksOfMessage;
     }
 
-    public String signatureCreation(Integer k, Integer [] blocksOfMessage, Integer [] sk){ // Создание подписи
+    public String signatureCreation(Integer k, Integer [] blocksOfMessage, Integer [] sk){ // Creating a signature
         String SIGNATURE= "";
         for(int i = 0; i < k; i++){
             SIGNATURE += sk[blocksOfMessage[i]];
