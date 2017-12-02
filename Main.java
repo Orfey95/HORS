1dd19dea4f29eff2bde1d957a9bf8a3eaa16c93e
Sasha <sasha7692@gmail.com>
2017-12-02 23:29:25 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,55 @@
-import Config.*;
-
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        KeyPairGeneration kpg = new KeyPairGeneration();
         SignatureGeneration sg = new SignatureGeneration();
-        Binarylog bl = new Binarylog();
+        SignatureVerification sv = new SignatureVerification();
 
         String Message = "Hello";
         int S = 128;
-        int t = 256;
-        int k = 16;
-        System.out.println(Arrays.asList(sg.separateMessage(Message, k, t)));
+
+        Scanner inL = new Scanner(System.in);
+        System.out.printf("Input parametr l (length of pseudo random sequence):\n"); //128
+        int l = Integer.parseInt(inL.nextLine());
+
+        Scanner inK = new Scanner(System.in);
+        System.out.printf("Input parametr k (S(mod k) = 0):\n"); //16
+        int k = Integer.parseInt(inK.nextLine());
+
+        int t = (int) Math.pow(2, (S / k)); //256
+        long start = System.currentTimeMillis(); // STAR TIMER
+        kpg.generatePairKey(t, l);
+        System.out.println("X: " + KeyPairGeneration.X);
+        System.out.println("Y: " + KeyPairGeneration.Y);
+        System.out.println("SIGNATURE: " + sg.signatureCreation(k, t, l, Message));
+        System.out.println("Y`: " + sv.hashSignature(k, l));
+        long finish = System.currentTimeMillis(); // FINISH TIMER
+        long timeConsumedMillis = finish - start;
+        //Statistic
+        System.out.println("\n" + "Statictic:");
+        System.out.println("Length of Private key: " + KeyPairGeneration.X.length() + " bits");
+        System.out.println("Length of Public key: " + KeyPairGeneration.Y.length() + " bits");
+        System.out.println("Length of Signature: " + SignatureGeneration.SIGNATURE.length() + " bits");
+        System.out.println("Time: " + timeConsumedMillis + "ms" + "\n");
+        //Statistic
+        if(sv.verification(Message, k, t, S, l))
+            System.out.println("Signature is valid");
+        else
+            System.out.println("Signature is NOT valid");
+        Scanner inExit = new Scanner(System.in);
+        System.out.printf("Exit or not? (0 - Exit, 1 - Start):\n");
+        int exitOrNot = Integer.parseInt(inExit.nextLine());
+        if(exitOrNot == 1){
+            Reboot();
+            main(args);
+        }
+    }
+
+    public static void Reboot(){ // Reboot global parametrs
+        KeyPairGeneration.X = "";
+        KeyPairGeneration.Y = "";
+        SignatureGeneration.SIGNATURE = "";
     }
 }
